1296aa2f7eb1d92ff7c160e20a0bc2d1d3954507
Derik <deriksadiki@gmail.com>
2019-02-07 10:44:17 +0200

    with key

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+declare var firebase;
 
 @Component({
   selector: 'app-sign-in',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
